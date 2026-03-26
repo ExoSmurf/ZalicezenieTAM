4b795b3b2d4c2da2ccb9b1dcacf31e1a860611e7
Exo <beboczek447@gmail.com>
2026-03-26 18:03:08 +0100

    Lab4 - dodawanie taskow, stylizacja taskow

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
+
+  List<Task> task = [
+    Task(title: "spotkanie z wojtkiem", deadline: "jutro", done: true),
+    Task(title: "wojna", deadline: "dzis", done: false),
+    Task(title: "flutter", deadline: "po-jutrze", done: false),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +24,80 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(child: Text("Masz dziś ${task.length} zadania",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ))),
+                  SizedBox(height: 16),
+                  Center(child: Text("Dzisiejsze zadania")),
+                  SizedBox(height: 16),
+                ],
+              ),
               Text("KrakFlow"),
               Text("Organizacja studiów"),
               Text("Dzisiejsze zadania"),
-            ],
-        ),),
+              SizedBox(height: 100),
+              Expanded(
+                  child: ListView.builder(
+                    itemCount: task.length,
+                    itemBuilder: (context, index) {
+                      return TaskCard(
+                          title: task[index].title,
+                          subtitle: task[index].deadline,
+                          icon: Icons.eighteen_mp_sharp,
+                          done: task[index].done
+                      );
+                    }),)
+                ],
+              ),
+          ),
+        ),
+      );
+  }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+
+  Task({required this.title, required this.deadline, required this.done});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final bool done;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.done,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 100),
+      child: Card(
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(
+            title,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Row(children: [Text(subtitle), Text(done.toString())]),
+        ),
       ),
     );
   }
